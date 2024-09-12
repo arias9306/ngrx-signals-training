99c5902f38d8715f97df3db8a7c13d4eed4c9118
Andres Arias <arias@media-interactive.de>
2024-09-12 15:16:59 +0200

    chore: add the basic scaffold for the app

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'todo',
+    loadChildren: () => import('./todo/todo.routes').then((r) => r.todoRoutes),
+  },
+  {
+    path: 'recipe',
+    loadChildren: () =>
+      import('./recipe/recipe.routes').then((r) => r.recipeRoutes),
+  },
+];
---
+++ src/app/recipe/recipe.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import { RecipePageComponent } from './recipe-page/recipe-page.component';
+
+export const recipeRoutes: Routes = [
+  {
+    path: '',
+    component: RecipePageComponent,
+  },
+];
