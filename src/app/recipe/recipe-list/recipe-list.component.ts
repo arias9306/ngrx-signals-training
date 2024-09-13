dcd4597e82a4ddc245b524cc9510ee5c17ae7d28
Andres Arias <arias@media-interactive.de>
2024-09-13 10:15:17 +0200

    feat: add list module in the todo and recipe list

--- src/app/recipe/recipe-list/recipe-list.component.ts
+++ src/app/recipe/recipe-list/recipe-list.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { MatListModule } from '@angular/material/list';
+import { Recipe } from '../model/recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   standalone: true,
-  imports: [],
+  imports: [MatListModule],
   templateUrl: './recipe-list.component.html',
   styleUrl: './recipe-list.component.css',
 })
-export class RecipeListComponent {}
+export class RecipeListComponent {
+  recipes: Recipe[] = [];
+}
--- src/app/todo/todo-list/todo-list.component.ts
+++ src/app/todo/todo-list/todo-list.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { MatListModule } from '@angular/material/list';
+import { Todo } from '../model/todo.model';
 
 @Component({
   selector: 'app-todo-list',
   standalone: true,
-  imports: [],
+  imports: [MatListModule],
   templateUrl: './todo-list.component.html',
-  styleUrl: './todo-list.component.css'
+  styleUrl: './todo-list.component.css',
 })
 export class TodoListComponent {
-
+  todos: Todo[] = [];
 }
