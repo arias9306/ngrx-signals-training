34fb37637efcab843961a894597fcc24d06373cd
Andres Arias <arias@media-interactive.de>
2024-09-13 09:26:14 +0200

    feat: add the navigation component

--- src/app/recipe/recipe-page/recipe-page.component.ts
+++ src/app/recipe/recipe-page/recipe-page.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { RecipeListComponent } from '../recipe-list/recipe-list.component';
 
 @Component({
   selector: 'app-recipe-page',
   standalone: true,
-  imports: [RecipeListComponent],
+  imports: [RecipeListComponent, MatToolbarModule],
   templateUrl: './recipe-page.component.html',
   styleUrl: './recipe-page.component.css',
 })
--- src/app/todo/todo-page/todo-page.component.ts
+++ src/app/todo/todo-page/todo-page.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { TodoListComponent } from '../todo-list/todo-list.component';
 
 @Component({
   selector: 'app-todo-page',
   standalone: true,
-  imports: [TodoListComponent],
+  imports: [TodoListComponent, MatToolbarModule],
   templateUrl: './todo-page.component.html',
   styleUrl: './todo-page.component.css',
 })
